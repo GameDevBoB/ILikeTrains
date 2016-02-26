a19085054a5dba454f624b36163b85b418260f9c
Diego Gurrieri <diegogurrieri@live.com>
2016-02-26 23:32:49 +0100

    Test OnMouseOver sulle trappole
    Test

---
+++ Assets/Scripts/Traps.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Traps : MonoBehaviour {
+
+	public int ColliderRay=10;
+
+	void Awake() {
+		GetComponent<SphereCollider>().radius = ColliderRay;
+		this.GetComponent<Collider>().enabled = true;
+	}
+	
+
+	void Update () {
+		
+	}
+
+
+	void OnMouseOver()
+	{
+		Debug.Log ("Ciao");
+		if (Input.GetMouseButtonDown (0))
+			this.GetComponent<Collider> ().enabled = true;
+		
+}
+
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Enemy") {
+
+		}
+	}
+
+}
