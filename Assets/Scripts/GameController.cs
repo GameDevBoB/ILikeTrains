ff77f6eede9023cfe55f21dbbc9dd8dada04337e
GameDevBoB <r_tolomei@hotmail.com>
2016-02-26 18:52:20 +0100

    Cambiamenti al gamecontroller implementazione prime variabili per la gestione delle trappole e la gui

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,12 +7,15 @@ public class GameController : MonoBehaviour
     public static GameController instance;
     [HideInInspector]
     public GameObject[] enemies;
-    public GameObject enemy;
-    public GameObject[] traps;
+    public GameObject enemyPrefab;
+    public GameObject[] trapsPrefab;
     [HideInInspector]
     public GameObject[] trapsInArray;
     public bool isPaused = true;
     private int enemyArrayCounter;
+    private bool trapIsBeingPlaced;
+    public bool isPlaceable;
+    private int trapIndex;
     // Use this for initialization
     void Start()
     {
@@ -26,37 +29,65 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (trapIsBeingPlaced)
+        {
+            SelectTrap(trapIndex);
+            PlaceTrap();
+            
+            
+        }
+        
     }
 
     void SpawnEnemies()
     {
         for (int i = 0; i < enemies.Length; i++)
         {
-            enemies[i] = Instantiate(enemy, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-            enemy.SetActive(false);
+            enemies[i] = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            enemyPrefab.SetActive(false);
         }
     }
 
     void InstantiateTraps()
     {
-        for (int i = 0; i < traps.Length; i++)
+        for (int i = 0; i < trapsPrefab.Length; i++)
         {
-            trapsInArray[i] = Instantiate(traps[i], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            trapsInArray[i] = Instantiate(trapsPrefab[i], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
             trapsInArray[i].SetActive(false);
         }
     }
 
-    public void PlaceTraps(int index)
+    public void SelectTrap(int index)
     {
-
+        trapIsBeingPlaced = true;
+        trapIndex = index;
         var mousePos = Input.mousePosition;
-        trapsInArray[index].transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        trapsInArray[index].transform.position = new Vector3(Camera.main.ScreenToWorldPoint(mousePos).x,0, Camera.main.ScreenToWorldPoint(mousePos).y);
+
     }
 
-    public void GetEnemy()
+    public GameObject GetEnemy()
     {
-        enemies[enemyArrayCounter].SetActive(true);
+        //enemies[enemyArrayCounter].SetActive(true);
+        int actualCounter = enemyArrayCounter;
         enemyArrayCounter++;
+        if (enemyArrayCounter <= enemies.Length)
+        {
+            enemyArrayCounter++;
+            
+        }
+        else
+        {
+            enemyArrayCounter = 0;
+        }
+        return enemies[actualCounter];
+    }
+
+    public void PlaceTrap()
+    {
+        if (Input.GetMouseButton(0) && isPlaceable)
+        {
+            trapIsBeingPlaced = false;
+        }
     }
 }
