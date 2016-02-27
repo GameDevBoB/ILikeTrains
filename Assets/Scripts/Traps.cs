d7aa8ab69fd7c9a1f10eafc56b92600f34d28455
DenMig90 <miglinodenis@gmail.com>
2016-02-27 01:24:32 +0100

    Sistemato Trappola
    Sistemato onmouseover e danno ai nemici

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -4,31 +4,43 @@ using System.Collections;
 public class Traps : MonoBehaviour {
 
 	public int ColliderRay=10;
+    public float damage;
+    public float explosionCooldown;
+
+    private SphereCollider myTrigger;
+    private float explosionStart;
 
 	void Awake() {
-		GetComponent<SphereCollider>().radius = ColliderRay;
-		this.GetComponent<Collider>().enabled = true;
+        myTrigger = GetComponent<SphereCollider>();
+		myTrigger.radius = ColliderRay;
+		//this.GetComponent<SphereCollider>().enabled = true;
+        Physics.queriesHitTriggers = false;
+        explosionStart = 0;
 	}
 	
 
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (myTrigger.radius < ColliderRay)
+            myTrigger.radius += 0.5f;
+        else
+            myTrigger.enabled = false;
+    }
 
-
-	void OnMouseOver()
-	{
-		Debug.Log ("Ciao");
-		if (Input.GetMouseButtonDown (0))
-			this.GetComponent<Collider> ().enabled = true;
-		
-}
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && (((Time.time - explosionStart) > explosionCooldown) || explosionStart==0))
+        {
+            myTrigger.enabled = true;
+            explosionStart = Time.time;
+            myTrigger.radius = 0;
+        }
+    }
 
 
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "Enemy") {
-
-		}
+            col.gameObject.SendMessage("GetDamage", damage);
+        }
 	}
 
 }
