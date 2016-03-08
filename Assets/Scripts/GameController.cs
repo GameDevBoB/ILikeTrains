fddc3c884f3f091c32403b10be9a9e52ef315c57
GameDevBoB <r_tolomei@hotmail.com>
2016-03-08 23:02:45 +0100

    fixed trap instantiation issues

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -125,7 +125,7 @@ public class GameController : MonoBehaviour
         //
         {
             //WE INSTANTIATE THE SELECTED TRAP
-            selectedTrap = Instantiate(trapsPrefabs[index], Vector3.zero, trapsPrefabs[index].transform.rotation) as GameObject;
+            selectedTrap = Instantiate(trapsPrefabs[index], new Vector3(0,-1,0), trapsPrefabs[index].transform.rotation) as GameObject;
             selectedTrapCost = trapsCosts[index];
             GUIController.instance.DeactivateInstanceButton();
         }
