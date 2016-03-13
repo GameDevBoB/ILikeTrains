b7ebd1577b739cf7aad7459e43adef0985d53662
Diego Gurrieri <diegogurrieri@live.com>
2016-03-13 15:54:07 +0100

    Menu Principale con Titolo,Play,Exit
    (Play rimanda alla scena "SelectionMenu" quindi la scena di selezione
    livelli è quella e Chiara deve lavorare direttamente su quella)

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadSelectionLevelMenu(){
+		Application.LoadLevel ("LevelSelection");
+	}
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
