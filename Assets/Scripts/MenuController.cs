2efcc6ecc1438e6127ee54b5d4ef9d31308c9388
Diego Gurrieri <diegogurrieri@live.com>
2016-03-13 16:35:10 +0100

    Menu con Titolo,Play,Select Level,Exit

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
-	public void LoadSelectionLevelMenu(){
+	public void LoadMainGame(){
 		Application.LoadLevel ("Denis");
 	}
 	public void ExitGame(){
 		Application.Quit ();
 	}
+	public void LoadSelectionLevelMenu(){
+		Application.LoadLevel ("LevelSelection");
+	}
+
 }
