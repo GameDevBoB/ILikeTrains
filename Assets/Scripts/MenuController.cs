e6864ac75a444730b9aa00b84a383e0f6a303306
GameDevBoB <r_tolomei@hotmail.com>
2016-03-13 16:17:39 +0100

    Changes to menucontroller

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MenuController : MonoBehaviour {
 
 	public void LoadSelectionLevelMenu(){
-		Application.LoadLevel ("LevelSelection");
+		Application.LoadLevel ("Denis");
 	}
 	public void ExitGame(){
 		Application.Quit ();
