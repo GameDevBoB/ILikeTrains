61ba030bcf8bb082f32b3474aae3a53a0e33778d
Diego Gurrieri <diegogurrieri@live.com>
2016-03-16 13:24:14 +0100

    SoundController Test

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public static SoundController instance;
+	public AudioClip[]  TrapSound=new AudioClip[2];
+	public AudioClip[] Enemy=new AudioClip[3];
+ //Other sounds
+
+
+
+
+	void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}else
+		{
+			DestroyImmediate(gameObject);
+		}
+	}
+
+
+
+}
