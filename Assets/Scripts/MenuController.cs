878d26c7d0c8ea8b106a2ce824f229eee84343a3
GameDevBoB <r_tolomei@hotmail.com>
2016-03-26 03:00:12 +0100

    lsat changes before die :v:

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,42 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
     public UISlider myslider;
     private bool load = false;
-	public Texture2D cursorTexture;
-	private Vector2 hotSpot = Vector2.zero;
-	private string sceneToLoad;
-
+    public Texture2D cursorTexture;
+    private Vector2 hotSpot = Vector2.zero;
+    private string sceneToLoad;
+    
     void Start()
     {
-		Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
         myslider.value = 0;
         load = false;
     }
 
     void Update()
     {
-		//cursorTexture = camera.main.ScreenToViewportPoint(Input.mousePosition);
-        if(load)
+        //cursorTexture = camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (load)
         {
             myslider.value += 0.01f;
-            if(myslider.value >= 1)
-               Application.LoadLevel(sceneToLoad);
+            if (myslider.value >= 1)
+            {
+                Application.LoadLevel(sceneToLoad);
+                
+            }
         }
     }
 
-	public void LoadLevelScene(){
+    public void LoadLevelScene()
+    {
 
-		sceneToLoad = UIButton.current.name;
+        sceneToLoad = UIButton.current.name;
 
-	}
-	public void LoadMainGame(){
-		//Application.LoadLevel ("Denis");
-	}
-	public void ExitGame(){
-		Application.Quit ();
-	}
+    }
+    public void LoadMainGame()
+    {
+        //Application.LoadLevel ("Denis");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 
     public void Load()
     {
